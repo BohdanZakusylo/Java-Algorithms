dbd4cf4c8b62eeae76b57eec43609efc12eeec04
Zsombor Hajzer <zsombor.hajzer@student.nhlstenden.com>
2025-03-10 12:44:24 +0100

    added jar files and removed jar file exclusion in gitignoer

--- Algorithms/src/Main.java
+++ Algorithms/src/Main.java
@@ -19,15 +19,16 @@ public class Main
 //
 //        mbt.printTree();
 
-        QueueImplementation<Integer> queue = new QueueImplementation<Integer>();
-        Integer[] array = {1,5,7,3,4,6,1,43,5,67,2,3,765,34,23,543,7634,1234,5346,3,23,-666, 234, 123,543,654,7654,876,23,345,2,43,-234};
-        queue.addAll(List.of(array));
+//        QueueImplementation<Integer> queue = new QueueImplementation<Integer>();
+//        Integer[] array = {1,5,7,3,4,6,1,43,5,67,2,3,765,34,23,543,7634,1234,5346,3,23,-666, 234, 123,543,654,7654,876,23,345,2,43,-234};
+//        queue.addAll(List.of(array));
 
 //        QueueSorts.mergeSort(queue);
-        queue.printToArray();
-        queue = QueueSorts.selectionSort(queue);
-        queue.printToArray();
-
+//        queue.printToArray();
+//        queue = QueueSorts.selectionSort(queue);
+//        queue.printToArray()
+//        profileQueueMergeSort();
+        profileQueueSelectionSort();
 
 
     }
