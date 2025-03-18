55b597ab8b0a80398cd63dad64ea7fcf8401f396
Alex Schuepbach <alex.schupbach@student.nhlstenden.com>
2025-03-18 19:15:00 +0100

    Created MainMenu UI file
    Done using Java Swing

--- Algorithms/src/Main.java
+++ Algorithms/src/Main.java
@@ -1,6 +1,10 @@
+import UI.MainMenu;
+
 public class Main
 {
     public static void main(String[] args)
     {
+        MainMenu mainMenu = new MainMenu();
+        mainMenu.setVisible(true);
     }
 }
---
+++ Algorithms/src/UI/MainMenu.java
@@ -0,0 +1,21 @@
+package UI;
+
+import javax.swing.*;
+
+public class MainMenu extends JFrame {
+    private JButton queueButton;
+    private JButton stackButton;
+    private JButton quitButton;
+    private JButton importDataButton;
+    private JPanel contentPane;
+    private JButton binarySearchTreeButton;
+
+    public MainMenu()
+    {
+        setTitle("Something");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setContentPane(contentPane);
+        pack();
+    }
+
+}
